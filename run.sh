7357e3f2f0904cac1c551e806053255f5c793909
Ondřej Němeček <ondrej.nemecek@gmail.com>
2025-09-16 15:24:18 +0200

    Library package build OK

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+rm -rfv .angular &&
 rm -rfv dist &&
 rm -rfv src/message-api &&
 java -jar util/openapi-generator-cli.jar validate -i src/message-api.json &&
 java -jar util/openapi-generator-cli.jar generate -i src/message-api.json -g typescript-angular -o src/message-api &&
 npm install &&
-ng build &&
-npm cache clean --force &&
-npm pack --pack-destination dist &&
-npm install --no-save --no-fund ./dist/message-api-0.0.1.tgz &&
+npm run-script build &&
+npm pack ./dist/message-api --pack-destination dist &&
+#---
+# to publish to network repo:
+# npm publish ./dist/message-api --access public &&
+#---
 echo "OK" ||
 echo "ER" ;
 
